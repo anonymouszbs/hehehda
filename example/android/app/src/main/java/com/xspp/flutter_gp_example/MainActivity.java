46b2fd8305b9998e674d540bbbaeaa64559553cf
like-chinaese <1556397100@qq.com>
2020-03-26 17:58:13 +0800

    呵呵呵

--- example/android/app/src/main/java/com/xspp/flutter_gp_example/MainActivity.java
+++ example/android/app/src/main/java/com/xspp/flutter_gp_example/MainActivity.java
@@ -3,15 +3,14 @@ package com.xspp.flutter_gp_example;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
 import android.os.Bundle;
 
 
 import androidx.annotation.NonNull;
 
-import java.nio.channels.Channel;
 
-import io.flutter.embedding.android.FlutterActivity;
+
+
 import io.flutter.embedding.engine.FlutterEngine;
 import io.flutter.plugin.common.EventChannel;
 import io.flutter.plugin.common.MethodCall;
