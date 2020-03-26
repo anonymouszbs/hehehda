261252daf3d9051328eed561e8fddca6d51e7add
like-chinaese <1556397100@qq.com>
2020-03-26 12:57:32 +0800

    cuihan测试

---
+++ ios/Classes/FlutterGpPlugin.h
@@ -0,0 +1,4 @@
+#import <Flutter/Flutter.h>
+
+@interface FlutterGpPlugin : NSObject<FlutterPlugin>
+@end
---
+++ ios/Classes/FlutterGpPlugin.m
@@ -0,0 +1,20 @@
+#import "FlutterGpPlugin.h"
+
+@implementation FlutterGpPlugin
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+  FlutterMethodChannel* channel = [FlutterMethodChannel
+      methodChannelWithName:@"flutter_gp"
+            binaryMessenger:[registrar messenger]];
+  FlutterGpPlugin* instance = [[FlutterGpPlugin alloc] init];
+  [registrar addMethodCallDelegate:instance channel:channel];
+}
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+  if ([@"getPlatformVersion" isEqualToString:call.method]) {
+    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  } else {
+    result(FlutterMethodNotImplemented);
+  }
+}
+
+@end
